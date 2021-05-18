86fc15c36c7580882d26698b5a4a06d05fe1da48
Franlly Mendoza Fana <franlly_mendoza@ucne.edu.do>
2021-05-17 23:48:04 -0400

    Add project files.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Mi_Primer_Registro_completo_en_blazor.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mi_Primer_Registro_completo_en_blazor.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Personas> Personas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = Data\Personas.db");
+        }
+    }
+}
---
+++ Models/Personas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mi_Primer_Registro_completo_en_blazor.Models
+{
+    public class Personas
+    {
+        [Key]
+        public int PersonaId { get; set; }
+        public string Nombres { get; set; }
+        public string Telefono { get; set; }
+        public string Cedula { get; set; }
+        public string Direccion { get; set; }
+        public DateTime Fecha { get; set; }
+
+
+    }
+}
